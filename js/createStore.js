50fa1f4816a6881d79688ff537a7d73e3cf812b2
caicindy87 <caicindy87@gmail.com>
2020-05-28 16:30:01 -0700

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,48 @@
-let state;
+// wrap state in function to protect from accidental reassignment
+function createStore(reducer) {
+  let state;
+
+  // state is now accessible to dispatch
+  // just created a closure by moving dispatch inside another function
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  // need to expose state so we can retrieve it outside this function
+  function getState() {
+    return state;
+  }
+
+  // need to expose dispatch method to the public because we'll need to call it when certain events happen
+  // returned object called store
+  return { dispatch: dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case "INCREASE_COUNT":
       return { count: state.count + 1 };
 
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+}
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
+}
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+// dispatch({ type: "@@INIT" });
+let button = document.getElementById("button");
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+// store contains all our app's state and a dispatch method
+// can dispatch actions that modify state
+// createStore takes a reducer as an argument
+let store = createStore(reducer);
+store.dispatch({ type: "@@INIT" });
+
+button.addEventListener("click", function () {
+  store.dispatch({ type: "INCREASE_COUNT" });
+});
